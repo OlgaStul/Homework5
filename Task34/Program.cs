7c0b60062bd6ba4a6276c36cf1f46739c17de563
Olga <chi89@yandex.ru>
2023-01-31 23:38:49 +0300

    Определяет количество чётных чисел в массиве

---
+++ Task34/Program.cs
@@ -0,0 +1,49 @@
+﻿/*  Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+//узнаем длину массива от пользователя, переводим в число
+int LengthArray(string message)
+{
+    int dimension = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out dimension)) break;
+        else Console.WriteLine("Ввели не корректное число, повторите ввод: ");
+    }
+    return Math.Abs(dimension);
+}
+
+//инициализируем массив
+int[] InitArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random rnd = new Random();
+    for (int i = 0; i < dimension; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);        
+    }
+    return arr;
+}
+
+//количество четных чисел в массиве
+int NumberOfEvenNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
+}
+
+int dimension = LengthArray("Задайте колличество элементов в массиве: ");
+int[] array = InitArray(dimension);
+int evenNumber = NumberOfEvenNumbers(array);
+
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine($"Колличество четных элеменов в массиве - {evenNumber}");
